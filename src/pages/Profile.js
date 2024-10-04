8ca6c9b5c5b8575abe6e2c126e94af43b16983a1
NafisFuad106 <107258321+NafisFuadShahid@users.noreply.github.com>
2024-10-05 00:02:04 +0600

    upload or remove photo feature

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { doc, getDoc } from "firebase/firestore";
-import { db } from "../firebaseConfig";
+import { doc, onSnapshot, updateDoc } from "firebase/firestore";
+import {
+  deleteObject,
+  getDownloadURL,
+  ref,
+  uploadBytes,
+} from "firebase/storage";
+import { db, storage, auth } from "../firebaseConfig";
 import { FaUserAlt, FaCloudUploadAlt } from "react-icons/fa";
 import moment from "moment";
 
@@ -11,31 +17,79 @@ const monthAndYear = (date) =>
 const Profile = () => {
   const { id } = useParams();
   const [user, setUser] = useState();
+  const [img, setImg] = useState("");
 
   const getUser = async () => {
-    const docSnap = await getDoc(doc(db, "users", id));
-    if (docSnap.exists()) {
-      setUser(docSnap.data());
-    }
+    // const docSnap = await getDoc(doc(db, "users", id));
+    // if (docSnap.exists()) {
+    //   setUser(docSnap.data());
+    // }
+    const unsub = onSnapshot(doc(db, "users", id), (querySnapshot) =>
+      setUser(querySnapshot.data())
+    );
+
+    return () => unsub();
   };
+
+  const uploadImage = async () => {
+    // create image reference
+    const imgRef = ref(storage, `profile/${Date.now()} - ${img.name}`);
+    if (user.photoUrl) {
+      await deleteObject(ref(storage, user.photoPath));
+    }
+    // upload image
+    const result = await uploadBytes(imgRef, img);
+    // get download url
+    const url = await getDownloadURL(ref(storage, result.ref.fullPath));
+    // update user doc
+    await updateDoc(doc(db, "users", auth.currentUser.uid), {
+      photoUrl: url,
+      photoPath: result.ref.fullPath,
+    });
+    setImg("");
+  };
+
   useEffect(() => {
     getUser();
-  }, []);
-  console.log(user);
+    if (img) {
+      uploadImage();
+    }
+  }, [img]);
+
+  const deletePhoto = async () => {
+    const confirm = window.confirm("Delete photo permanently?");
+    if (confirm) {
+      await deleteObject(ref(storage, user.photoPath));
+      await updateDoc(doc(db, "users", auth.currentUser.uid), {
+        photoUrl: "",
+        photoPath: "",
+      });
+    }
+  };
+
   return user ? (
     <div className="mt-5 container row">
       <div className="text-center col-sm-2 col-md-3">
-        <FaUserAlt size={50} />
-        <div class="dropdown my-3 text-center">
+        {user.photoUrl ? (
+          <img
+            src={user.photoUrl}
+            alt={user.name}
+            style={{ widht: "100px", height: "100px", borderRadius: "50%" }}
+          />
+        ) : (
+          <FaUserAlt size={50} />
+        )}
+
+        <div className="dropdown my-3 text-center">
           <button
-            class="btn btn-secondary btn-sm dropdown-toggle"
+            className="btn btn-secondary btn-sm dropdown-toggle"
             type="button"
             data-bs-toggle="dropdown"
             aria-expanded="false"
           >
             Edit
           </button>
-          <ul class="dropdown-menu">
+          <ul className="dropdown-menu">
             <li>
               <label htmlFor="photo" className="dropdown-item">
                 <FaCloudUploadAlt size={30} /> Upload Photo
@@ -45,9 +99,14 @@ const Profile = () => {
                 id="photo"
                 accept="image/*"
                 style={{ display: "none" }}
+                onChange={(e) => setImg(e.target.files[0])}
               />
             </li>
-            <li className="dropdown-item btn">Remove Photo</li>
+            {user.photoUrl ? (
+              <li className="dropdown-item btn" onClick={deletePhoto}>
+                Remove Photo
+              </li>
+            ) : null}
           </ul>
         </div>
         <p>Member since {monthAndYear(user.createdAt.toDate())}</p>
